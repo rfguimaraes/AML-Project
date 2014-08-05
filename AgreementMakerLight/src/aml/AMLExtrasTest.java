ba9aa1bc99fc339eb2cf805fe2ff3a9ba771d1f6
Ricardo F. Guimarães <r_guimaraes92@hotmail.com>
2014-08-05 11:19:10 -0300

    Relocating code from Dictionary to the new Matcher.

--- AgreementMakerLight/src/aml/AMLExtrasTest.java
+++ AgreementMakerLight/src/aml/AMLExtrasTest.java
@@ -1,25 +1,31 @@
+/******************************************************************************
+ * Copyright 2014-2014 Ricardo F. Guimarães                                    *
+ *                                                                             *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may     *
+ * not use this file except in compliance with the License. You may obtain a   *
+ * copy of the License at http://www.apache.org/licenses/LICENSE-2.0           *
+ *                                                                             *
+ * Unless required by applicable law or agreed to in writing, software         *
+ * distributed under the License is distributed on an "AS IS" BASIS,           *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.    *
+ * See the License for the specific language governing permissions and         *
+ * limitations under the License.                                              *
+ *                                                                             *
+ *******************************************************************************
+ * Dictionary of translations obtain from the English Wiktionary, using        *
+ * wik2dict                                                                    *
+ *                                                                             *
+ * @authors Ricardo F. Guimarães                                               *
+ * @date 30-07-2014                                                            *
+ * @version 0.4                                                                *
+ ******************************************************************************/
+
 package aml;
 
-import java.util.List;
-
-import aml.util.Dictionary;
-import aml.util.DictionaryWord;
+import aml.match.WikDictMatcher;
 
 public class AMLExtrasTest {
     public static void main(String[] args) {
-        long time = System.currentTimeMillis() / 1000;
-        Dictionary d = new Dictionary();
-        time = System.currentTimeMillis() / 1000 - time;
-        System.out.println("Dict" + " loaded in " + time + " seconds");
-        System.out.println(d.translate("en", "dog", "pt"));
-        System.out.println(d.translate("pt", "cachorro", "en"));
-        System.out.println(d.translate("en", "dog", "es"));
-        System.out.println(d.translate("es", "perro", "pt"));
-        System.out.println(d.translate("pt", "saudade", "en"));
-        System.out.println(d.translate("pt", "saudade", "ko"));
-        System.out.println(d.translate("pt", "saudade", "jp"));
-        System.out.println(d.getAllTranslations("en", "dog"));
-        System.out.println();
-
+        WikDictMatcher wikmatch = new WikDictMatcher();
     }
 }
