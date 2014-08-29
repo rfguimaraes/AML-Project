50abb868fea21cb9303d6611f95d0fb0aa747807
Ricardo F. Guimarães <r_guimaraes92@hotmail.com>
2014-08-29 14:07:46 -0300

    Trying a non-boolean approach.

--- AgreementMakerLight/src/aml/match/dlmatch/DLMatcher.java
+++ AgreementMakerLight/src/aml/match/dlmatch/DLMatcher.java
@@ -29,6 +29,7 @@ import aml.match.Mapping;
 import aml.match.SecondaryMatcher;
 import aml.ontology.RelationshipMap;
 import aml.settings.MappingRelation;
+import org.semanticweb.elk.util.collections.Pair;
 
 import java.util.Iterator;
 import java.util.Set;
@@ -69,20 +70,32 @@ public class DLMatcher implements SecondaryMatcher {
 
             if(targetImpliesSource && !sourceImpliesTarget) {
                 parentsRelation = MappingRelation.SUPERCLASS;
+                System.out.println("Super");
             }
             else if(targetImpliesSource && sourceImpliesTarget) {
                 parentsRelation = MappingRelation.EQUIVALENCE;
+                System.out.println("Equiv");
             }
             else if(!targetImpliesSource && sourceImpliesTarget) {
                 parentsRelation = MappingRelation.SUBCLASS;
+                System.out.println("Sub");
             }
             else {
                 parentsRelation = MappingRelation.UNKNOWN;
+                System.out.println("Unk");
+            }
+            Pair <Integer, Double> res;
+            res = mappingsSourceTarget(a, sourceIds, targetIds);
+            if (res.getFirst() >= 2*sourceIds.size() / 3 && res.getSecond() >
+                    0.5) {
+                parentsRelation = MappingRelation.SUPERCLASS;
             }
 
             if (parentsRelation != MappingRelation.UNKNOWN) {
-                sourceParents = relationshipMap.getParents(c.getSourceId());
-                targetParents = relationshipMap.getParents(c.getTargetId());
+                sourceParents = relationshipMap.getAncestors(c.getSourceId(),
+                        1);
+                targetParents = relationshipMap.getAncestors(c.getSourceId(),
+                        1);
 
                 for (Integer sourceId : sourceParents) {
                     for (Integer targetId : targetParents) {
@@ -103,6 +116,23 @@ public class DLMatcher implements SecondaryMatcher {
             }
         }
         return true;
+    }
+
+    private Pair<Integer, Double> mappingsSourceTarget(Alignment a,
+                                             Set<Integer> sourceIds,
+                                          Set<Integer> targetIds) {
+        int mp = 0;
+        double sim = 0;
+        for (Integer sourceId : sourceIds) {
+            for (Integer targetId : targetIds) {
+                double t = a.getSimilarity(sourceId, targetId);
+                if (t > 0.0) {
+                    mp++;
+                    sim += t;
+                }
+            }
+        }
+        return new Pair<Integer, Double>(mp, sim/mp);
     }
 
     private boolean existsSourceMapping(Alignment a, int sourceId,
